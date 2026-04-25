373f1931b6e66e7710edcc40f3cf0e0d2b548b35
Lenovo <bakosterit@gmail.com>
2026-04-25 18:06:24 +0300

    sitemap eklendi

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://cezaevihaklari.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cezaevihaklari.com/kararlar",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cezaevihaklari.com/dilekceler",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cezaevihaklari.com/istatistikler",
+      lastModified: new Date(),
+    },
+  ];
+}
