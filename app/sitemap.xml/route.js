062a44428191ebc663f10140d2bc31a79e7ecb95
Lenovo <bakosterit@gmail.com>
2026-06-04 11:06:29 +0300

    Add..

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,35 @@
+const siteUrl = "https://cezaevihaklari.com";
+
+export const dynamic = "force-dynamic";
+
+function createSitemapIndex(sitemaps = []) {
+  const body = sitemaps
+    .map((item) => {
+      return `
+  <sitemap>
+    <loc>${item.loc}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>`;
+    })
+    .join("");
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${body}
+</sitemapindex>`;
+}
+
+export async function GET() {
+  const sitemaps = [
+    { loc: `${siteUrl}/sitemap-sabit.xml` },
+    { loc: `${siteUrl}/sitemap-ictihatlar.xml` },
+    { loc: `${siteUrl}/sitemap-yazilar.xml` },
+    { loc: `${siteUrl}/sitemap-mevzuat.xml` },
+    { loc: `${siteUrl}/sitemap-kararlar.xml` },
+  ];
+
+  return new Response(createSitemapIndex(sitemaps), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
